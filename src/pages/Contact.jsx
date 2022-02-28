855474faf4806936d95ec76d50bd9931798abb19
Azabs <azabs123@gmail.com>
2022-02-28 20:51:51 -0300

    added some stuff to the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,29 @@
 const Contact = () => {
   return (
     <>
-      <div>
-        <form method="post">
+      <div className="text-center my-sm-4 row">
+        <div className="col-md-6 bg-primary text-light p-3">
+          <h3>Contact Us</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quid
+            possumus facere melius? Quod non faceret, si in voluptate summum
+            bonum poneret. Sic enim censent, oportunitatis esse beate vivere.
+            Duo Reges: constructio interrete. Illis videtur, qui illud non
+            dubitant bonum dicere -; Paria sunt igitur. Quis negat? Nam et a te
+            perfici istam disputationem volo, nec tua mihi oratio longa videri
+            potest. Nescio quo modo praetervolavit oratio. Solum praeterea
+            formosum, solum liberum, solum civem, stultost;{" "}
+          </p>
+        </div>
+        <form method="post" id="contact-form" className="col-md-6 my-4">
           <div>
             <input
-              className="con_input1"
               name="name"
               required="required"
               type="text"
               placeholder="Name"
             />
             <input
-              className="con_input1"
               name="mail"
               required="required"
               type="text"
@@ -22,14 +33,12 @@ const Contact = () => {
 
           <div>
             <input
-              className="con_input2"
               name="phone-number"
               required="required"
               type="text"
               placeholder="Phone Number"
             />
             <input
-              className="con_input2"
               name="asunto"
               required="required"
               type="text"
@@ -40,7 +49,7 @@ const Contact = () => {
             name="message"
             required="required"
             placeholder="Write your message"
-            cols="30"
+            cols="44"
             rows="10"
           ></textarea>
 
